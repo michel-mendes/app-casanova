99c4c5d737105b9e86a501f57ff5fb61a2aca284
Michel Mendes <michel.gmsm@gmail.com>
2024-07-18 00:12:28 -0300

    feat: adiciona animação durante transição entre páginas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { TopNavigation } from "./components/TopNavigation";
 import "./globals.css";
+import { Suspense } from "react";
+import { LoadingAnimation } from "./components/LoadingAnimation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <TopNavigation />
-        {children}
+        <Suspense fallback={<LoadingAnimation />}>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
